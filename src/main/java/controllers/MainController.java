155440f132b198aaa00c85951b3318db7620c4ce
tara <tara.mck@hotmail.com>
2018-07-28 14:09:30 +0100

    added heroku changes

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -12,11 +12,13 @@ import java.util.HashMap;
 import java.util.List;
 
 import static spark.Spark.get;
+import static spark.SparkBase.port;
 import static spark.SparkBase.staticFileLocation;
 
 public class MainController {
 
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
 
         staticFileLocation("/public");
 
@@ -24,13 +26,21 @@ public class MainController {
         UserController userController = new UserController();
         Seeds.seedData();
 
+
         get("/manager", (req, res) -> {
             HashMap<String, Object> model = new HashMap<>();
             model.put("template", "templates/home.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
-
     }
+
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+	}
 }
 
